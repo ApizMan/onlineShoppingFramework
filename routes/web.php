16b408ab13a815a18cc941a7755da1e5799cc137
Nafiz Mansor <nafizmansor@gmail.com>
2023-01-09 11:48:10 +0800

    Payment Method

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
+Route::get('', function () {
     return view('welcome');
+});
+
+Route::get('/payment-method', function () {
+    return view('payment_method');
 });
